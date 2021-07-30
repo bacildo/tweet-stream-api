f74935b21d456c50d2a493ad76b7858d0b24f449
bacildo <diogo.silva@m2msolutions.com.br>
2021-07-30 17:19:46 -0300

    adição da opção de envio de gif no Objeto

--- tweetUsingSingleString.js
+++ tweetUsingSingleString.js
@@ -2,10 +2,11 @@ const twitterClient = require("./init");
 
 // Uma forma de postar informações através de uma string de forma manual.
 
-let tweet = "#botApiTweeting";
+let tweet = "#Bbot é flamenguista!";
 twitterClient.tweets
   .statusesUpdate({
     status: tweet,
+    attachment_url: "https://twitter.com/i/status/1420899024601862150", //a URL precisa ser um permalink do domínio do twitter
   })
   .then((response) => {
     console.log("Tweet sent!", response);
